79d883e5fcf1757892c0a02e6940de3a4506c3df
Matheus M Almeida <matheus.mma2015@gmail.com>
2025-11-24 21:00:55 -0300

    add reserva

--- estacionamento/src/main/java/projeto_integrador/estacionamento/excecoesPersonalisadas/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package projeto_integrador.estacionamento.excecoesPersonalisadas;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
